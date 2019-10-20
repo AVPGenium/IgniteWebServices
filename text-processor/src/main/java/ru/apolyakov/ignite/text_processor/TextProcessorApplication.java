2c2fe5fad72ba3744d22a58a78457be078fa1cf9
Поляков Андрей Владимирович <avpgenium@yandex.ru>
2019-10-21 01:33:16 +0300

    add microservices structure with maven multimodule and apache ignite

--- text-processor/src/main/java/ru/apolyakov/ignite/text_processor/TextProcessorApplication.java
+++ text-processor/src/main/java/ru/apolyakov/ignite/text_processor/TextProcessorApplication.java
@@ -1,17 +1,19 @@
-package ru.apolyakov.apacheignitetest;
+package ru.apolyakov.ignite.text_processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
-public class ApacheIgniteTestApplication {
+@ComponentScan("ru.apolyakov.ignite.text_processor.config")
+public class TextProcessorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApacheIgniteTestApplication.class, args);
+		SpringApplication.run(TextProcessorApplication.class, args);
 	}
 
 }
---
+++ text-processor/src/main/java/ru/apolyakov/ignite/text_processor/config/IgniteProvider.java
@@ -0,0 +1,44 @@
+package ru.apolyakov.ignite.text_processor.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteIllegalStateException;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetSocketAddress;
+import java.util.Collections;
+
+import static org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi.DFLT_PORT;
+
+@Configuration
+public class IgniteProvider {
+    private Log log = LogFactory.getLog(IgniteProvider.class);
+    private final Ignite ignite;
+    private boolean started = false;
+
+    public IgniteProvider() {
+        try {
+            Ignition.ignite("testGrid-client");
+            started = true;
+        } catch (IgniteIllegalStateException e) {
+            log.debug("Using the Ignite instance that has been already started.");
+        }
+        if (started)
+            ignite = Ignition.ignite("testGrid-client");
+        else {
+            ignite = Ignition.start("ignite.xml");
+            ((TcpDiscoverySpi) ignite.configuration().getDiscoverySpi())
+                    .getIpFinder()
+                    .registerAddresses(Collections.singletonList(new InetSocketAddress("localhost", DFLT_PORT)));
+        }
+    }
+
+    @Bean
+    public Ignite getIgnite() {
+        return ignite;
+    }
+}
